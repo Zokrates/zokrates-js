3a97d13ce95227faff06ce57360be35adb7c2e68
dark64 <darem966@gmail.com>
2019-09-24 22:35:55 +0200

    Update ci

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export TAG="latest"
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build --file Dockerfile --tag blockchainit/zokrates-js:$TAG .
-docker push blockchainit/zokrates-js:$TAG
